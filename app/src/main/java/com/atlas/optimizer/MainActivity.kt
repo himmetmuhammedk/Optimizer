456a920c152c7e58a98779d101f01732889f4be8
himmetmuhammedk <himmetmuhammedk@gmail.com>
2026-03-25 21:08:08 +0300

    Last Sync: 2026-03-25 21:08 (Mobile)

--- app/src/main/java/com/atlas/optimizer/MainActivity.kt
+++ app/src/main/java/com/atlas/optimizer/MainActivity.kt
@@ -109,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         runOnUiThread {
             txtLog.append("\n$text")
             
-            // Scroll to bottom safely
+            // Scroll to bottom using the parent ScrollView
             val scrollView = txtLog.parent.parent as? android.widget.ScrollView
             scrollView?.post {
                 scrollView.fullScroll(android.view.View.FOCUS_DOWN)
